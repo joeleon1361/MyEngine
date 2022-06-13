143eb582c0158a19f1767d36bba875211270469c
joe_leon1361 <k020g1147@g.neec.ac.jp>
2022-06-13 14:54:52 +0900

    03_03完了

--- PostEffect.h
+++ PostEffect.h
@@ -7,7 +7,16 @@ public:
     // コンストラクタ
     PostEffect();
 
+    // 初期化
+    void Initialize();
+
     // 描画コマンドの発行
     void Draw(ID3D12GraphicsCommandList* cmdList);
+
+    // テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff;
+
+    // SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
--- Sprite.h
+++ Sprite.h
@@ -8,7 +8,7 @@
 // スプライト
 class Sprite
 {
-private: // エイリアス
+protected: // エイリアス
 	// Microsoft::WRL::を省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 	// DirectX::を省略
