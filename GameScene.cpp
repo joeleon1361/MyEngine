1806c94121a6c6b98f7e39434a2cd77c01245f3c
joe_leon1361 <k020g1147@g.neec.ac.jp>
2022-07-12 10:46:23 +0900

    コード整理

--- GameScene.cpp
+++ GameScene.cpp
@@ -174,8 +174,8 @@ void GameScene::Draw()
 	Object3d::PreDraw(cmdList);
 
 	// 3Dオブクジェクトの描画
-	/* objSkydome->Draw();
-	 objGround->Draw();*/
+	objSkydome->Draw();
+	// objGround->Draw();
 	objPlayer->Draw();
 
 	// testobject->Draw(cmdList);
