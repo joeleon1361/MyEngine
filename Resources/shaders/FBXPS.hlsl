c398d15a96014a4f5f144f68444ceb1ce10acc9a
joe_leon1361 <k020g1147@g.neec.ac.jp>
2022-07-17 20:25:59 +0900

    04_06完了

--- Resources/shaders/FBXPS.hlsl
+++ Resources/shaders/FBXPS.hlsl
@@ -16,7 +16,25 @@ float SchlickFresnel(float f0, float f90, float cosine)
 	return lerp(f0, f90, m5);
 }
 
+float DistributionGGX(float alpha, float NdotH)
+{
+	float alpha2 = alpha * alpha;
+	float t = NdotH * NdotH * (alpha2 - 1.0f) + 1.0f;
+	return alpha2 / (PI * t * t);
+}
 
+float3 CookTorranceSpecular(float NdotL, float NdotV, float NdotH, float LdotH)
+{
+	float Ds = DistributionGGX(roughness * roughness, NdotH);
+
+	float3 Fs = float3(1, 1, 1);
+
+	float Gs = 0.1f;
+
+	float m = 4.0f * NdotL * NdotV;
+
+	return Ds * Fs * Gs / m;
+}
 
 float3 BRDF(float3 L, float3 V)
 {
@@ -49,7 +67,9 @@ float3 BRDF(float3 L, float3 V)
 
 	float3 diffuseColor = diffuseReflectance * Fd * baseColor * (1 - metalness);
 
-	return diffuseColor;
+	float3 specularColor = CookTorranceSpecular(NdotL, NdotV, NdotH, LdotH);
+
+	return diffuseColor + specularColor;
 }
 
 struct PSOutput
