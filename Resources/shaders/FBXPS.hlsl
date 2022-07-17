dd454d6a2493d98d556a868a446e6a03221761d3
joe_leon1361 <k020g1147@g.neec.ac.jp>
2022-07-17 20:14:25 +0900

    04_05完了

--- Resources/shaders/FBXPS.hlsl
+++ Resources/shaders/FBXPS.hlsl
@@ -8,6 +8,16 @@ static const float PI = 3.141592654f;
 
 static float3 N;
 
+float SchlickFresnel(float f0, float f90, float cosine)
+{
+	float m = saturate(1 - cosine);
+	float m2 = m * m;
+	float m5 = m2 * m2 * m;
+	return lerp(f0, f90, m5);
+}
+
+
+
 float3 BRDF(float3 L, float3 V)
 {
 	float NdotL = dot(N, L);
@@ -19,9 +29,25 @@ float3 BRDF(float3 L, float3 V)
 		return float3(0, 0, 0);
 	}
 
+	float3 H = normalize(L + V);
+
+	float NdotH = dot(N, H);
+
+	float LdotH = dot(L, H);
+
 	float diffuseReflectance = 1.0f / PI;
 
-	float3 diffuseColor = diffuseReflectance * NdotL * baseColor * (1 - metalness);
+	float energyBias = 0.5f * roughness;
+	float Fd90 = energyBias + 2 * LdotH * LdotH * roughness;
+
+	float FL = SchlickFresnel(1.0f, Fd90, NdotL);
+
+	float FV = SchlickFresnel(1.0f, Fd90, NdotV);
+
+	float energyFactor = lerp(1.0f, 1.0f / 1.51f, roughness);
+	float Fd = FL * FV * energyFactor;
+
+	float3 diffuseColor = diffuseReflectance * Fd * baseColor * (1 - metalness);
 
 	return diffuseColor;
 }
@@ -48,3 +74,4 @@ float4 main(VSOutput input) : SV_TARGET
 	}
 	return float4(finalRGB, 1);
 }
+
